bef87e028365da2b075d6fc82d70cb0604770099
Trevor Coleman <trevor@trevorcoleman.design>
2022-03-26 12:17:03 -0400

    add cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,75 +4,84 @@ import {google, oauth2_v2} from "googleapis";
 import {getAuthUrl} from "./api-calls/google-auth";
 import Cookies from "cookies";
 import {getFreeBusy} from "./api-calls/free-busy";
+import _cors from "cors";
 
+const cors = _cors({origin: ["http://localhost:19006"]});
 
 export const auth = functions.https.onRequest(async (request, response) => {
-  console.log("auth");
-  const {auth, authenticated, token} = await authenticate(request, response);
+  cors(request, response, async ()=>{
+    console.log("auth");
+    const {auth, authenticated, token} = await authenticate(request, response);
 
-  let user: oauth2_v2.Schema$Userinfo | undefined;
-  if (!authenticated) {
-    response.status(401).json({authUrl: getAuthUrl()});
-    return;
-  }
-
-  if (token) {
-    const cookies = new Cookies(request, response);
-    cookies.set("token", JSON.stringify(token), {
-      expires: new Date(token.expiry_date ?? new Date(0)),
-      httpOnly: true,
-    });
-
-    google.options({auth});
-
-    try {
-      const userRes = await google.oauth2("v2").userinfo.get({});
-      console.log(userRes.data);
-      user = userRes?.data;
-    } catch (e) {
-      console.log("error");
+    let user: oauth2_v2.Schema$Userinfo | undefined;
+    if (!authenticated) {
+      response.status(401).json({authUrl: getAuthUrl()});
+      return;
     }
-  }
 
-  response.status(200).json({user, authUrl: getAuthUrl()});
+    if (token) {
+      const cookies = new Cookies(request, response);
+      cookies.set("token", JSON.stringify(token), {
+        expires: new Date(token.expiry_date ?? new Date(0)),
+        httpOnly: true,
+      });
+
+      google.options({auth});
+
+      try {
+        const userRes = await google.oauth2("v2").userinfo.get({});
+        console.log(userRes.data);
+        user = userRes?.data;
+      } catch (e) {
+        console.log("error");
+      }
+    }
+
+    response.status(200).json({user, authUrl: getAuthUrl()});
+  });
 });
 
+// eslint-disable-next-line max-len
 export const calendars = functions.https.onRequest(
     async (request, response) => {
-      const {auth, authenticated, token} = await authenticate(
-          request,
+      cors(request, response, async () => {
+        const {auth, authenticated, token} = await authenticate(
+            request,
+            response
+        );
+
+        if (!authenticated) {
           response
-      );
+              .status(401)
+              .send("You do not have permission to access this resource.");
+          return;
+        }
+        const calendar = await google.calendar({version: "v3", auth});
 
-      if (!authenticated) {
-        response
-            .status(401)
-            .send("You do not have permission to access this resource.");
-        return;
-      }
-      const calendar = await google.calendar({version: "v3", auth});
+        const list = await calendar.calendarList.list();
 
-      const list = await calendar.calendarList.list();
+        const primaryRes = await calendar.calendars.get(
+            {calendarId: "primary"}
+        );
 
-      const primaryRes = await calendar.calendars.get({calendarId: "primary"});
+        const {data: primary} = primaryRes;
 
-      const {data: primary} = primaryRes;
+        const {
+          data: {items},
+        } = list;
 
-      const {
-        data: {items},
-      } = list;
+        if (token) {
+          const cookies = new Cookies(request, response);
+          cookies.set("token", JSON.stringify(token), {
+            expires: new Date(token.expiry_date ?? 0),
+            httpOnly: true,
+          });
+        }
 
-      if (token) {
-        const cookies = new Cookies(request, response);
-        cookies.set("token", JSON.stringify(token), {
-          expires: new Date(token.expiry_date ?? 0),
-          httpOnly: true,
+        response.status(200).json({
+          primary,
+          calendars: items,
         });
-      }
-
-      response.status(200).json({
-        primary,
-        calendars: items,
       });
     });
 
